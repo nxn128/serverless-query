6277edacd12e2296f965275b1db5f674d4f320bf
Nick Nolasco <nick.nolasco@gmail.com>
2023-05-13 12:04:13 -0400

    feat: add basic data catalog

--- src/smallquery/functions/upload_data/aws/s3_wrapper.py
+++ src/smallquery/functions/upload_data/aws/s3_wrapper.py
@@ -1,4 +1,5 @@
 import boto3
+import json
 import os
 import requests
 
@@ -8,6 +9,7 @@ class S3Wrapper:
     Wraps AWS S3 interactions
     """
     BUCKET_NAME = os.getenv('DATA_BUCKET_NAME')
+    metadata_path = 'metadata/table.json'
 
     def __init__(self):
         self._s3 = boto3.resource('s3')
@@ -24,3 +26,17 @@ class S3Wrapper:
         except Exception as e:
             print(f'unable to upload file from {from_url} to {to_path}: {e}')
             raise
+
+    def save_metadata(self, bucket: str, data: dict):
+        obj = self._s3.Object(
+            bucket_name=bucket,
+            key=S3Wrapper.metadata_path,
+        )
+        obj.put(Body=json.dumps(data))
+
+    def read_metadata(self, bucket: str, path: str) -> str:
+        obj = self._s3.Object(
+            bucket_name=bucket,
+            key=S3Wrapper.metadata_path,
+        )
+        return obj.get()['Body'].read().decode('utf-8')
--- src/upload.py
+++ src/upload.py
@@ -1,12 +1,37 @@
 import click
+import json
 import os
 
 # TODO: lambda wrapper should not be under a specific func
 # we should use lambda layers for these shared libs
 from smallquery.functions.run_query.aws.lambda_wrapper import LambdaWrapper
+from smallquery.functions.upload_data.aws.s3_wrapper import S3Wrapper
 from smallquery.functions.upload_data.model.upload import Upload
 
 UPLOAD_FUNCTION_NAME = 'serverless-query-njn-UploadDataFunction'
+
+BUCKET = 'query-data-2ade95b0-e4b4-11ed-9186-0644e23de3ed'
+
+
+def save_metadata(data: dict):
+    s3 = S3Wrapper()
+    # os.environ['DATA_BUCKET_NAME'] = 's3://query-data-2ade95b0-e4b4-11ed-9186-0644e23de3ed'
+    s3.save_metadata(BUCKET, data)
+
+
+def save_table_name(table_name, to):
+    # 's3://query-data-2ade95b0-e4b4-11ed-9186-0644e23de3ed/uploads/taxi_2019_04.parquet
+    # os.environ['QUERY_TABLE_NAME'] = table_name
+    # os.environ['QUERY_TABLE_PATH'] = f's3://query-data-2ade95b0-e4b4-11ed-9186-0644e23de3ed/{to}'
+    data = {
+        'table_name': table_name,
+        'path': f"'s3://query-data-2ade95b0-e4b4-11ed-9186-0644e23de3ed/{to}'",
+    }
+    save_metadata(data)
+
+
+    #with open('./table_data.json', 'w') as f:
+    #    f.write(json.dumps(data))
 
 
 @click.command()
@@ -22,13 +47,21 @@ UPLOAD_FUNCTION_NAME = 'serverless-query-njn-UploadDataFunction'
     required=True,
     help='Bucket path and filename'
 )
-def upload(from_url: str, to: str):
+@click.option(
+    '--tablename',
+    '-n',
+    required=True,
+    help='Friendly table name'
+)
+def upload(from_url: str, to: str, tablename: str):
     """
     CLI entry point function
     """
     data = Upload(from_url, to).as_dict()
     lambda_wrapper = LambdaWrapper()
     lambda_wrapper.invoke_function(UPLOAD_FUNCTION_NAME, data, True)
+
+    save_table_name(tablename, to)
 
     print(f'Uploaded {from_url} to your data bucket at {to}')
 
